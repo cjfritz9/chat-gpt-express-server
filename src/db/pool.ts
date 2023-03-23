f4853d1ecb86a394880cd552ca84a946ebdfdd8a
CJ <cjfritz9@gmail.com>
2023-03-23 12:25:55 -0600

    implementation of cloud sql and authentication

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -2,10 +2,14 @@ import dotenv from 'dotenv';
 import pkg from 'pg';
 dotenv.config();
 
+const connectionString = process.env.NODE_ENV === 'production'
+? process.env.DB_URL
+: 'https://localhost:5432/chat-app';
+
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: 'https://localhost:5432/chat-app',
+  connectionString,
   ssl:
     process.env.NODE_ENV === 'production'
       ? { rejectUnauthorized: false }
